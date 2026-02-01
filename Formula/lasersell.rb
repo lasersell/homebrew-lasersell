4896e73553e5d0186dc8daa50e299e3d7ed0aa9e
lasersell-ci <ci@lasersell.app>
2026-02-01 14:21:29 +0000

    lasersell 0.2.2

--- Formula/lasersell.rb
+++ Formula/lasersell.rb
@@ -2,27 +2,27 @@ class Lasersell < Formula
   desc "LaserSell CLI daemon for automated exits on Solana trades"
   homepage "https://lasersell.io"
   license "Proprietary"
-  version "0.2.1"
+  version "0.2.2"
 
   on_macos do
     on_arm do
-      url "https://lasersell.github.io/apt/binaries/lasersell/0.2.1/lasersell_0.2.1_darwin_arm64.tar.gz"
-      sha256 "fc381f481204af8387933b260581535a9c8ebaf665fb57a2d4767215874fd30e"
+      url "https://lasersell.github.io/apt/binaries/lasersell/0.2.2/lasersell_0.2.2_darwin_arm64.tar.gz"
+      sha256 "a90e5aae36f97722ce1ec4b506ecfc6dc2aa96010e2a6bc7066e2826f41a9736"
     end
     on_intel do
-      url "https://lasersell.github.io/apt/binaries/lasersell/0.2.1/lasersell_0.2.1_darwin_amd64.tar.gz"
-      sha256 "bc894c3e8101ef755117b5be02fd93b4beec5dd5df41fde96176f38629214d5b"
+      url "https://lasersell.github.io/apt/binaries/lasersell/0.2.2/lasersell_0.2.2_darwin_amd64.tar.gz"
+      sha256 "afaf0331dfbd686b95bbf43cf5597fcf89dc89ebfd40b34bd6232c6be86fb7a9"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://lasersell.github.io/apt/binaries/lasersell/0.2.1/lasersell_0.2.1_linux_arm64.tar.gz"
-      sha256 "2d574e1efd31fd5b5b2e23ad86a8c0936174f2d514eb1393ee2c00afd1954232"
+      url "https://lasersell.github.io/apt/binaries/lasersell/0.2.2/lasersell_0.2.2_linux_arm64.tar.gz"
+      sha256 "b84e396a2a197c7c39d96cb0fcf89b8a50a70416c6335cdca73ef874f7e3dfd1"
     end
     on_intel do
-      url "https://lasersell.github.io/apt/binaries/lasersell/0.2.1/lasersell_0.2.1_linux_amd64.tar.gz"
-      sha256 "584342e289f5d14fdd9213edbfc6c405799949291f716a2beda81a8f715c1b24"
+      url "https://lasersell.github.io/apt/binaries/lasersell/0.2.2/lasersell_0.2.2_linux_amd64.tar.gz"
+      sha256 "fd24a9b316db7541ce31ed3d04a0ceffc9d889ea8da4b254f7b217a70ee96e8a"
     end
   end
 
