d58a902782ec0356aec33db8d3a9bcded2ca8dfc
haccer <22013186+haccer@users.noreply.github.com>
2026-02-25 09:14:07 -0500

    Fix endpoints

--- Formula/lasersell.rb
+++ Formula/lasersell.rb
@@ -6,22 +6,22 @@ class Lasersell < Formula
 
   on_macos do
     on_arm do
-      url "https://dl.lasersell.io/apt/binaries/lasersell/0.2.2/lasersell_0.2.2_darwin_arm64.tar.gz"
+      url "https://dl.lasersell.io/binaries/lasersell/0.2.2/lasersell_0.2.2_darwin_arm64.tar.gz"
       sha256 "b05c51ac30cba9378038ef153c1af88a7e1bd18ccc372eb56e8ad7573584c3ca"
     end
     on_intel do
-      url "https://dl.lasersell.io/apt/binaries/lasersell/0.2.2/lasersell_0.2.2_darwin_amd64.tar.gz"
+      url "https://dl.lasersell.io/binaries/lasersell/0.2.2/lasersell_0.2.2_darwin_amd64.tar.gz"
       sha256 "5b42aaca0e24448738ee84f7eb10950264d5992ac81e5e1e855829fb3d5d9b9b"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://dl.lasersell.io/apt/binaries/lasersell/0.2.2/lasersell_0.2.2_linux_arm64.tar.gz"
+      url "https://dl.lasersell.io/binaries/lasersell/0.2.2/lasersell_0.2.2_linux_arm64.tar.gz"
       sha256 "f86bdf77bb5288de8f0449a5aa522c2adaa6dea9d71e82480cc774fb521330dd"
     end
     on_intel do
-      url "https://dl.lasersell.io/apt/binaries/lasersell/0.2.2/lasersell_0.2.2_linux_amd64.tar.gz"
+      url "https://dl.lasersell.io/binaries/lasersell/0.2.2/lasersell_0.2.2_linux_amd64.tar.gz"
       sha256 "da1fb91a52725f5ac21797a86bf2746068ee32a7e166e847e221699696daca69"
     end
   end
@@ -35,7 +35,7 @@ class Lasersell < Formula
   end
 
   livecheck do
-    url "https://dl.lasersell.io/apt/binaries/lasersell/latest.txt"
+    url "https://dl.lasersell.io/binaries/lasersell/latest.txt"
     regex(/(\d+\.\d+\.\d+)/)
   end
 end
